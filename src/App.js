d970eacda36cd6dbf97731d531eb6e2e3fbcf34f
Jim Lusk <jblusk@gmail.com>
2020-09-24 19:10:46 -0700

    Removed the extra gi character.

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <table>
         <tr>
-          <th>Name</th>gi
+          <th>Name</th>
           <th>Price</th>
           <th>Shares Held</th>
           <th>Market Value</th>
